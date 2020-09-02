9ef4b4db3d0d7f418eafc356dc809b38e762efd9
tongyazhou <yazhou5969@163.com>
2020-09-02 13:36:55 +0800

    update

---
+++ src/com/base/test/string/TestString.java
@@ -0,0 +1,13 @@
+package com.base.test.string;
+
+/**
+ * @author: TongYaZhou
+ * @create: 2020-09-02 13:20
+ **/
+public class TestString {
+
+    public static void main(String[] args) {
+        String s = new String();
+        s.equals("1");
+    }
+}
